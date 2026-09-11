f4b3759c193b02d29c70105033c0c1f6c1922eaa
Guillaume Galinanes <galinanesgm@gmail.com>
2026-09-11 13:53:56 +0200

    (US03) add readAllActive to incidentRepository

--- server/src/modules/incident/incidentActions.ts
+++ server/src/modules/incident/incidentActions.ts
@@ -1,10 +1,21 @@
 import type { RequestHandler } from "express";
 import { StatusCodes } from "http-status-codes";
+
 import incidentRepository from "./incidentRepository";
+
+// Only BREAD here (Browse, Read, Edit, Add, Delete)
+
+const DEFAULT_LIST_LIMIT = 15;
+const MAX_LIST_LIMIT = 100;
 
 const browse: RequestHandler = async (req, res, next) => {
 	try {
-		const incidents = await incidentRepository.readAll();
+		const requested =
+			Number.parseInt(req.query.limit as string, 10) ||
+			DEFAULT_LIST_LIMIT;
+		const limit = Math.min(requested, MAX_LIST_LIMIT);
+
+		const incidents = await incidentRepository.readAllActive(limit);
 		res.status(StatusCodes.OK).json(incidents);
 	} catch (err) {
 		next(err);
