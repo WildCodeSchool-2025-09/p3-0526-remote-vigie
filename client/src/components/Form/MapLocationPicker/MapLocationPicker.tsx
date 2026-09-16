9dc04e2f9f7a57488e7be18f2fe5c9f59dfbb2ef
Laurent Koehler <laurent.koehler@gmail.com>
2026-09-16 11:24:22 +0200

    (US01) handle tile error

--- client/src/components/Form/MapLocationPicker/MapLocationPicker.tsx
+++ client/src/components/Form/MapLocationPicker/MapLocationPicker.tsx
@@ -4,6 +4,7 @@ import L from "leaflet";
 import { renderToStaticMarkup } from "react-dom/server";
 import { MapContainer, Marker, TileLayer, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { useState } from "react";
 
 type MapLocationPickerProps = {
 	value: Position;
@@ -38,6 +39,8 @@ export default function MapLocationPicker({
 	draggable = true,
 	className = "w-full",
 }: MapLocationPickerProps) {
+	const [hasTileError, setHasTileError] = useState(false);
+
 	return (
 		<div className={className}>
 			<div
@@ -52,6 +55,13 @@ export default function MapLocationPicker({
 					attributionControl={draggable}
 				>
 					<TileLayer
+						key={String(hasTileError)}
+						eventHandlers={{
+							tileerror: () => {
+								if (hasTileError === true) return;
+								setHasTileError(true);
+							},
+						}}
 						attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 						url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
 					/>
@@ -78,6 +88,18 @@ export default function MapLocationPicker({
 					</a>{" "}
 					contributors
 				</p>
+			)}
+			{hasTileError && (
+				<div className="w-full">
+					<p className="text-primary ">Erreur sur la carte</p>
+					<button
+						type="button"
+						className="btn btn-accent btn-md grow rounded-full border-none px-5 font-bold"
+						onClick={() => setHasTileError(false)}
+					>
+						Réessayer
+					</button>
+				</div>
 			)}
 		</div>
 	);
