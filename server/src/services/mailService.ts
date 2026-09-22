b45dd215249a4d33d2eaf1b2df84db5a3fd36a23
Laurent Koehler <laurent.koehler@gmail.com>
2026-09-22 14:48:08 +0200

    (US01) mailService.ts

---
+++ server/src/services/mailService.ts
@@ -0,0 +1,30 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: false,
+    auth: {
+        user: process.env.SMTP_USERNAME,
+        pass: process.env.SMTP_PASSWORD,
+    },
+});
+
+async function send({
+    to,
+    subject,
+    html,
+}: {
+    to: string;
+    subject: string;
+    html: string;
+}) {
+    await transporter.sendMail({
+        from: "Vigie <no-reply@vigie.app>",
+        to,
+        subject, 
+        html,
+    });
+}
+
+export default { send };
